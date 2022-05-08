6e379c424386d06ee39ddca0f9ed376b2f17519d
Krushnapanchal8 <86007243+Krushnapanchal8@users.noreply.github.com>
2022-05-08 10:46:56 +0530

    Add files via upload

---
+++ Dicee_App/Dicee_App/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Dicee_App
+//
+//  Created by Mac on 02/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        
+    }
+
+    @IBAction func rollButton(_ sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+
+    }
+    
+}
+
